ea8a3abd9ba398bc3090d1224e470491467bf669
Torre Matthew <torre.matthew@gmail.com>
2018-08-28 22:37:57 -0400

    adding new button from input

--- assets/javascript/gify.js
+++ assets/javascript/gify.js
@@ -20,20 +20,38 @@ let buttonDisplay = [
     "dark",
 ];
 
+let button;
+
 
 function initialExperience () {
 
-    for (let i = 0; i < movie.length; i++) {
+    $(".buttons").empty();
 
-        let button = $("<button>")
-            .attr("type", "button")
-            .addClass("btn btn-sm btn-outline-" + buttonDisplay[i])
-            .append(movie[i]);
+        for (let i = 0; i < movie.length; i++) {
+
+            let button = $("<button>")
+                .attr("type", "button")
+                .addClass("btn btn-sm btn-outline-" + buttonDisplay[i])
+                .append(movie[i]);
 
             $(".buttons").append(button);
-    }
+        }
 }
 
+$(".add-new").on("click", function(){
+    event.preventDefault();
+ let newEntry = $(".form-control").val().trim();
+ 
+ console.log(newEntry); 
+
+ movie.push(newEntry);
+
+ console.log(movie);
+
+ initialExperience();
+
+
+});
 // Click handler for reaching out to the API once a button is clicked
 // Click handler for going from a still gif to an animated one and back
 // Function for creating a button based on value typed in an input
